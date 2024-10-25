fe980d1c72ba909fe2d307eb9d4492f52c31645a
squoshi <95097230+squoshi@users.noreply.github.com>
2024-10-25 15:21:00 -0500

    FUCK

--- src/main/java/com/squoshi/sailor/physics/Waves.java
+++ src/main/java/com/squoshi/sailor/physics/Waves.java
@@ -1,13 +1,18 @@
 package com.squoshi.sailor.physics;
 
+import com.squoshi.sailor.Sailor;
 import de.articdive.jnoise.pipeline.JNoise;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.apache.commons.lang3.tuple.Pair;
 import org.joml.Vector3d;
+import org.joml.Vector3dc;
 import org.joml.primitives.AABBdc;
+import org.joml.primitives.AABBic;
 import org.valkyrienskies.core.api.ships.LoadedServerShip;
 import org.valkyrienskies.mod.common.util.GameTickForceApplier;
 
@@ -24,47 +29,52 @@ public class Waves {
             ship.saveAttachment(GameTickForceApplier.class, new GameTickForceApplier());
         }
         GameTickForceApplier forceApplier = ship.getAttachment(GameTickForceApplier.class);
-        double mass = ship.getInertiaData().getMass();
-        AABBdc shipAABB = ship.getWorldAABB();
-        double x = shipAABB.maxX() - shipAABB.minX();
-        double z = shipAABB.maxZ() - shipAABB.minZ();
-        double centerX = x / 2;
-        double centerZ = z / 2;
-        List<Pair<Double, Vector3d>> forces = new ArrayList<>();
-        for (double xx = 0 - centerX; xx < x - 1; xx++) {
-            for (double zz = 0 - centerZ; zz < z - 1; zz++) {
-                Vector3d posFinal = new Vector3d(xx, shipAABB.minY(), zz);
-                Vector3d worldPos = new Vector3d(shipAABB.minX() + centerX + posFinal.x, shipAABB.minY(), shipAABB.minZ() + centerZ + posFinal.z);
-                double oceanHeight = getOceanHeightAt(worldPos.x, worldPos.z, level.getGameTime(), oceanNoise);
-                BlockHitResult clip = level.clip(new ClipContext(
-                    new Vec3(centerX + posFinal.x, posFinal.y, centerZ + posFinal.z),
-                    new Vec3(centerX + posFinal.x, posFinal.y - level.getMaxBuildHeight(), centerZ + posFinal.z),
-                    ClipContext.Block.COLLIDER, ClipContext.Fluid.WATER,
-                    null
-                ));
-                if (clip.getType() != BlockHitResult.Type.BLOCK) continue;
-                Vec3 location = clip.getLocation();
-                if (level.getFluidState(clip.getBlockPos()).isEmpty()) continue;
-                if ((location.y() + oceanHeight) > shipAABB.minY()) continue;
-                double vForce = Math.max(oceanHeight * (mass), 0);
-                forces.add(Pair.of(vForce, posFinal));
+
+//        double mass = ship.getInertiaData().getMass();
+        AABBdc worldAABB = ship.getWorldAABB();
+
+//        AABBic shipAABB = ship.getShipAABB();
+//        double worldSizeX = worldAABB.maxX() - worldAABB.minX();
+//        double worldSizeZ = worldAABB.maxZ() - worldAABB.minZ();
+//        double centerWorldX = worldSizeX / 2;
+//        double bottomCenterWorldY = worldAABB.minY();
+//        double centerWorldZ = worldSizeZ / 2;
+//        Vector3d shipCenterExample = convertToShipSpace(new Vector3d(centerWorldX, bottomCenterWorldY, centerWorldZ), ship);
+
+//        List<Pair<Vector3d, Double>> waveHeights = new ArrayList<>();
+        for (double x = worldAABB.minX(); x < worldAABB.maxX(); x++) {
+            for (double y = worldAABB.minY(); y < worldAABB.maxY(); y++) {
+                for (double z = worldAABB.minZ(); z < worldAABB.maxZ(); z++) {
+                    Sailor.LOGGER.info("Checking block at ({}, {}, {})", x, y, z);
+                    Vector3d pos = convertToShipSpace(new Vector3d(x, y, z), ship);
+//                    double oceanHeight = getOceanHeightAt(x, z, level.getGameTime(), oceanNoise);
+//                    if (oceanHeight < 0) continue;
+                }
             }
         }
-        if (forces.isEmpty()) return;
-        double vForce = 0;
-        Vector3d posFinal = new Vector3d();
-        for (Pair<Double, Vector3d> force : forces) {
-            if (force.getLeft() > vForce) {
-                vForce = force.getLeft();
-                posFinal = force.getRight();
-            }
-        }
-        if (vForce == 0) return;
-        Vector3d forceToApply = new Vector3d(0, vForce, 0);
-        forceApplier.applyInvariantForceToPos(forceToApply, posFinal);
+
+//        Vector3d averagePos = new Vector3d();
+//        double averageHeight = 0;
+//        for (Pair<Vector3d, Double> waveHeight : waveHeights) {
+//            averagePos.add(waveHeight.getLeft());
+//            averageHeight += waveHeight.getRight();
+//        }
+//        averagePos.div(waveHeights.size());
+//        averageHeight /= waveHeights.size();
+//
+//        forceApplier.applyInvariantForceToPos(
+//                new Vector3d(0, averageHeight * mass, 0),
+//                new Vector3d(averagePos.x(), averagePos.y(), averagePos.z())
+//        );
     }
 
     private static double getOceanHeightAt(double x, double z, long time, JNoise oceanNoise) {
         return oceanNoise.evaluateNoise(x * 1e-2, z * 1e-2, time * 1e-2);
+    }
+
+    private static Vector3d convertToShipSpace(Vector3d worldPos, LoadedServerShip ship) {
+        Vector3d pos = ship.getTransform().getWorldToShip().transformPosition(worldPos).sub(ship.getTransform().getPositionInShip());
+        Sailor.LOGGER.info("Converted world pos {} to ship pos {}", worldPos, pos);
+        return pos;
     }
 }
