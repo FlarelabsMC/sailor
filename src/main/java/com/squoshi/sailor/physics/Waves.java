1c47cd9b4d0933b624b22e23602fde2915fbf268
squoshi <95097230+squoshi@users.noreply.github.com>
2024-10-29 11:52:32 -0500

    intermission

--- src/main/java/com/squoshi/sailor/physics/Waves.java
+++ src/main/java/com/squoshi/sailor/physics/Waves.java
@@ -1,23 +1,19 @@
 package com.squoshi.sailor.physics;
 
-import com.squoshi.sailor.Sailor;
 import de.articdive.jnoise.pipeline.JNoise;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
 import org.apache.commons.lang3.tuple.Pair;
 import org.joml.Vector3d;
-import org.joml.Vector3dc;
 import org.joml.primitives.AABBdc;
-import org.joml.primitives.AABBic;
 import org.valkyrienskies.core.api.ships.LoadedServerShip;
 import org.valkyrienskies.mod.common.util.GameTickForceApplier;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.squoshi.sailor.util.MathUtil.convertBlockPosToShipyard;
+import static com.squoshi.sailor.util.MathUtil.convertToShipSpace;
 
 public class Waves {
     public static JNoise oceanNoise;
@@ -30,51 +26,57 @@ public class Waves {
         }
         GameTickForceApplier forceApplier = ship.getAttachment(GameTickForceApplier.class);
 
-//        double mass = ship.getInertiaData().getMass();
+        double mass = ship.getInertiaData().getMass();
         AABBdc worldAABB = ship.getWorldAABB();
 
-//        AABBic shipAABB = ship.getShipAABB();
-//        double worldSizeX = worldAABB.maxX() - worldAABB.minX();
-//        double worldSizeZ = worldAABB.maxZ() - worldAABB.minZ();
-//        double centerWorldX = worldSizeX / 2;
-//        double bottomCenterWorldY = worldAABB.minY();
-//        double centerWorldZ = worldSizeZ / 2;
-//        Vector3d shipCenterExample = convertToShipSpace(new Vector3d(centerWorldX, bottomCenterWorldY, centerWorldZ), ship);
-
-//        List<Pair<Vector3d, Double>> waveHeights = new ArrayList<>();
+        List<Pair<Vector3d, Double>> waveHeights = new ArrayList<>();
         for (double x = worldAABB.minX(); x < worldAABB.maxX(); x++) {
             for (double y = worldAABB.minY(); y < worldAABB.maxY(); y++) {
                 for (double z = worldAABB.minZ(); z < worldAABB.maxZ(); z++) {
-                    Sailor.LOGGER.info("Checking block at ({}, {}, {})", x, y, z);
-                    Vector3d pos = convertToShipSpace(new Vector3d(x, y, z), ship);
-//                    double oceanHeight = getOceanHeightAt(x, z, level.getGameTime(), oceanNoise);
-//                    if (oceanHeight < 0) continue;
+                    Vector3d posOnShip = convertToShipSpace(new Vector3d(x, y, z), ship);
+                    BlockPos blockPosOnShip = convertBlockPosToShipyard(new BlockPos((int) x, (int) y, (int) z), ship);
+
+                    if (level.getBlockState(blockPosOnShip).isAir()) continue;
+
+                    BlockPos bPos = new BlockPos((int) x, (int) y - 1, (int) z);
+                    boolean isEmpty = !level.getFluidState(bPos).isEmpty();
+                    if (isEmpty) continue;
+
+                    int times = 0;
+                    while (!isEmpty) {
+                        bPos = bPos.above();
+                        isEmpty = level.getFluidState(bPos).isEmpty();
+                        times++;
+                        if (times > 5) break;
+                    }
+                    if (!isEmpty) continue;
+
+                    double oceanHeight = getOceanHeightAt(x, z, level.getGameTime(), oceanNoise);
+                    Vector3d pos = new Vector3d(x, y + 1 + oceanHeight, z);
+
+                    if (y <= pos.y()) {
+                        waveHeights.add(Pair.of(posOnShip, oceanHeight));
+                    }
                 }
             }
         }
 
-//        Vector3d averagePos = new Vector3d();
-//        double averageHeight = 0;
-//        for (Pair<Vector3d, Double> waveHeight : waveHeights) {
-//            averagePos.add(waveHeight.getLeft());
-//            averageHeight += waveHeight.getRight();
-//        }
-//        averagePos.div(waveHeights.size());
-//        averageHeight /= waveHeights.size();
-//
-//        forceApplier.applyInvariantForceToPos(
-//                new Vector3d(0, averageHeight * mass, 0),
-//                new Vector3d(averagePos.x(), averagePos.y(), averagePos.z())
-//        );
+        Vector3d averagePos = new Vector3d();
+        double averageHeight = 0;
+        for (Pair<Vector3d, Double> waveHeight : waveHeights) {
+            averagePos.add(waveHeight.getLeft());
+            averageHeight += waveHeight.getRight();
+        }
+        averagePos.div(waveHeights.size());
+        averageHeight /= waveHeights.size();
+
+        forceApplier.applyInvariantForceToPos(
+                new Vector3d(0, averageHeight * mass, 0),
+                new Vector3d(averagePos.x(), averagePos.y(), averagePos.z())
+        );
     }
 
     private static double getOceanHeightAt(double x, double z, long time, JNoise oceanNoise) {
         return oceanNoise.evaluateNoise(x * 1e-2, z * 1e-2, time * 1e-2);
-    }
-
-    private static Vector3d convertToShipSpace(Vector3d worldPos, LoadedServerShip ship) {
-        Vector3d pos = ship.getTransform().getWorldToShip().transformPosition(worldPos).sub(ship.getTransform().getPositionInShip());
-        Sailor.LOGGER.info("Converted world pos {} to ship pos {}", worldPos, pos);
-        return pos;
     }
 }
--- src/main/java/com/squoshi/sailor/util/MathUtil.java
+++ src/main/java/com/squoshi/sailor/util/MathUtil.java
@@ -1,9 +1,34 @@
 package com.squoshi.sailor.util;
 
+import net.minecraft.core.BlockPos;
 import org.joml.Vector3d;
+import org.valkyrienskies.core.api.ships.LoadedServerShip;
 
 public class MathUtil {
     public static Vector3d project(Vector3d vec1, Vector3d vec2) {
         return vec1.mul(vec1.dot(vec2) / Math.pow(vec1.length(), 2));
+    }
+
+    /**
+     * Converts a world position to a position relative to the provided ship
+     * @param worldPos The position in the Minecraft world
+     * @param ship The ship to convert the position to
+     * @return The position in ship space (center of the ship is equal to 0,0,0)
+     */
+    public static Vector3d convertToShipSpace(Vector3d worldPos, LoadedServerShip ship) {
+        Vector3d vec = new Vector3d(worldPos);
+        return ship.getTransform().getWorldToShip().transformPosition(vec).sub(ship.getTransform().getPositionInShip());
+    }
+
+    /**
+     * Converts a block position to a position relative to the provided ship
+     * @param blockPos The block position in the Minecraft world
+     * @param ship The ship to convert the position to
+     * @return The block position in ship space (center of the ship is equal to 0,0,0)
+     */
+    public static BlockPos convertBlockPosToShipyard(BlockPos blockPos, LoadedServerShip ship) {
+        Vector3d vec = new Vector3d(blockPos.getX(), blockPos.getY(), blockPos.getZ());
+        Vector3d shipyardPos = ship.getTransform().getWorldToShip().transformPosition(vec);
+        return new BlockPos((int) shipyardPos.x, (int) shipyardPos.y, (int) shipyardPos.z);
     }
 }
